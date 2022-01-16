fa45cea56ed6a78b0462216805b33a9c2954468e
talakondaravi9494 <55018803+talakondaravi9494@users.noreply.github.com>
2022-01-16 21:55:45 +0530

    Add files via upload

---
+++ phase1assitedproject/ConstructorUses2.java
@@ -0,0 +1,15 @@
+package phase1assitedproject;
+
+public class ConstructorUses2 {
+	public int data;
+	public String name;
+	
+
+	public ConstructorUses2()
+	{
+		this.data=data;
+		this.name=name;
+	
+	}
+
+}
